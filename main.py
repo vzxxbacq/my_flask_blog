b891f13ab09b55108306956563b6fcb91dde22eb
vzxxbacq <vzxxbacq>
2018-09-05 17:54:49 +0800

    .

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
     env.globals.update(zip=zip)
     b = Blask(templateDir=settings.templateDir, postDir=settings.postDir, defaultLayout=settings.defaultLayout,
               staticDir=settings.staticDir, tittle=settings.tittle)
-    b.run(host='127.0.0.1', port=5000)
+    b.run(host='0.0.0.0', port=5000)
